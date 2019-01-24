e8b09e43b2d3c9ca88a7319b411959a651cf3283
Benson Wu <buwenson@gmail.com>
2019-01-23 16:38:12 -0800

    Added initial cleaning and summary stats. Not complete.

--- HW1.R
+++ HW1.R
@@ -3,7 +3,10 @@
 setwd("C:/Users/buwen/OneDrive/Desktop/MATH 189")
 
 #Load dependencies
+
+#example of how to install: install.packages("ggplot2")
 library(dplyr)
+library(ggplot2)
 
 #Load dataset
 df<-read.csv("babies23.txt", header = TRUE, sep="")
@@ -16,7 +19,35 @@ df<-read.csv("babies23.txt", header = TRUE, sep="")
 df_smoker<- df%>%filter(smoke==1)
 summary(df_smoker)
 
+df_smoker$age[df_smoker$age == 99] <- NA
+df_smoker$gestation[df_smoker$gestation == 999] <- NA
+#check gestation for 999 values
+#check race for 999 values
+#age 99
+#ed 9
+#ht 99
+#wt.1 999
+#drace 99
+#dage 99
+#ded 9
+#dht 99
+#dwt 999
+#marital 
+#inc goes to 9
+#smoke 0-3
+#time 0-9
+#number 0-9
+
+#Histogram
+ggplot(df_smoker, aes(wt)) + 
+  geom_histogram() + ggtitle("Weights of Smoker") + theme(plot.title = element_text(hjust = 0.5))
+  
+
 
 #Estimations for nonsmokers
 df_nonsmoker<- df%>%filter(smoke==0)
 summary(df_nonsmoker)
+
+#Histogram
+ggplot(df_nonsmoker, aes(wt)) + 
+  geom_histogram() + ggtitle("Weights of Nonsmoker") + theme(plot.title = element_text(hjust = 0.5))
