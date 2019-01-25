5b8a371c8f6867829614e274c500789a67a79e7a
Benson Wu <buwenson@gmail.com>
2019-01-25 00:34:57 -0800

    Reorganized all the plots so that they are organized by plot type

--- HW1.R
+++ HW1.R
@@ -1,8 +1,10 @@
 #Load working directory
 #Enter your own path in the quotes in the path variable
-path<-"/Users/Timlee/"
+# path<-"/Users/Timlee/"
+# path<-"C:/Users/Aiden/"
+# path<-"C:/Users/buwen/"
 setwd(paste0(path,'Documents/Git/M189WI2019'))
-setwd("C:/Users/Aiden/Desktop/UCSD/homework/math_189/case1/M189WI2019/")
+
 
 
 #Load dependencies
@@ -32,6 +34,7 @@ df$ded[df$ded == 9] <-NA
 df$dht[df$dht == 99] <-NA
 df$dwt[df$dwt == 999] <-NA
 df$inc[df$inc == 98] <-NA
+df$smoke[df$smoke==9]<-NA
 
 
 #Separate data into smokers and non smokers
@@ -49,71 +52,97 @@ summary(df_nonsmoker$outcome)
 #Find a way to print publishable summary statistics tables for our desired variables
 
 #################
-#####Smokers#####
+#####Plots#######
 #################
-#Histogram of weight
+
+###Histograms###
+#weight
 s_weight_hist<-ggplot(df_smoker, aes(wt)) + 
   geom_histogram(bins=20) + ggtitle("Smokers") + 
   theme(plot.title = element_text(hjust = 0.5)) +
-  labs(x = "Weight (oz.)")
+  labs(x = "Weight (oz.)") + xlim(50, 180)
 
-#Histogram of gestation 
+ns_weight_hist<-ggplot(df_nonsmoker, aes(wt)) + 
+  geom_histogram(bins=20) + ggtitle("Nonsmokers") + 
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(x = "Weight (oz.)") + xlim(50, 180)
+
+#Maybe add a count legend that changes color with higher counts in the bins
+#Side by side plot of Birth weights 
+grid.arrange(s_weight_hist, ns_weight_hist, ncol=2, top = textGrob("Birth Weights",gp=gpar(fontsize=20,font=3)))
+
+#gestation 
 s_gestation_hist<-ggplot(df_smoker, aes(gestation)) + 
   geom_histogram(bins=20) + ggtitle("Smokers") +
   theme(plot.title = element_text(hjust = 0.5)) +
   labs(x = "Days") + xlim(200, 350)
 
-#QQ plot of weight
-s_weight_qq<-ggplot(df_smoker, aes(sample = wt)) +
-  stat_qq() + stat_qq_line() + ggtitle("Smokers") +
-  theme(plot.title = element_text(hjust = 0.5)) 
-
-#QQ plot of gestation
-s_gestation_qq<-ggplot(df_smoker, aes(sample = gestation)) +
-  stat_qq() + stat_qq_line() + ggtitle("Smokers") +
-  theme(plot.title = element_text(hjust = 0.5)) 
-
-
-#################
-###Nonsmokers####
-#################
-#Histogram of weight
-ns_weight_hist<-ggplot(df_nonsmoker, aes(wt)) + 
-  geom_histogram(bins=20) + ggtitle("Nonsmokers") + 
-  theme(plot.title = element_text(hjust = 0.5)) +
-  labs(x = "Weight (oz.)")
-
-#Histogram of gestation  
 ns_gestation_hist<-ggplot(df_nonsmoker, aes(gestation)) + 
   geom_histogram(bins=20) + ggtitle("Nonsmokers") + 
   theme(plot.title = element_text(hjust = 0.5)) +
   labs(x = "Days") + xlim(200, 350)
 
-#QQ plot of weight
+grid.arrange(s_gestation_hist, ns_gestation_hist, ncol=2, top = textGrob("Gestation",gp=gpar(fontsize=20,font=3)))
+
+
+###QQ plots###
+#weight
+s_weight_qq<-ggplot(df_smoker, aes(sample = wt)) +
+  stat_qq() + stat_qq_line() + ggtitle("Smokers") +
+  theme(plot.title = element_text(hjust = 0.5)) 
+
 ns_weight_qq<-ggplot(df_nonsmoker, aes(sample = wt)) +
   stat_qq() + stat_qq_line() + ggtitle("Nonsmokers") +
   theme(plot.title = element_text(hjust = 0.5)) 
 
-#QQ plot of gestation
+grid.arrange(s_weight_qq, ns_weight_qq, ncol=2, top = textGrob("Birth Weights",gp=gpar(fontsize=20,font=3)))
+
+#gestation
+s_gestation_qq<-ggplot(df_smoker, aes(sample = gestation)) +
+  stat_qq() + stat_qq_line() + ggtitle("Smokers") +
+  theme(plot.title = element_text(hjust = 0.5)) 
+
 ns_gestation_qq<-ggplot(df_nonsmoker, aes(sample = gestation)) +
   stat_qq() + stat_qq_line() + ggtitle("Nonsmokers") +
   theme(plot.title = element_text(hjust = 0.5)) 
 
 
-#Histograms
-##Maybe add a count legend that changes color with higher counts in the bins
-#Side by side plot of Birth weights 
-grid.arrange(s_weight_hist, ns_weight_hist, ncol=2, top = textGrob("Birth Weights",gp=gpar(fontsize=20,font=3)))
+###Box plots###
+#Factorize smoke in order to plot correctly, and then make sure it's unfactorized after the boxplots
+#Need to rename X axis for the graphs make sense
+df$smoke<-factor(df$smoke)
 
-#Side by side plot of gestation
-grid.arrange(s_gestation_hist, ns_gestation_hist, ncol=2, top = textGrob("Gestation",gp=gpar(fontsize=20,font=3)))
+ggplot(df, aes(x=smoke, y=wt)) + 
+  geom_boxplot() + ggtitle("Birth Weight") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(x = "Smoking Status", y = "Weight (oz.)")
 
-#QQ plots
-#Side by side plot of Birth weights 
-grid.arrange(s_weight_qq, ns_weight_qq, ncol=2, top = textGrob("Birth Weights",gp=gpar(fontsize=20,font=3)))
+ggplot(df, aes(x=smoke, y=gestation)) + 
+  geom_boxplot() + ggtitle("Gestation time") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(x = "Smoking Status", y = "Days")
 
-#Side by side plot of gestation
 grid.arrange(s_gestation_qq, ns_gestation_qq, ncol=2, top = textGrob("Gestation",gp=gpar(fontsize=20,font=3)))
+
+#Make smoke continuous again
+df$smoke<-as.numeric(df$smoke)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
