5de6d90b6dc5beacc5ecb5db5a09f816b0b62425
Benson Wu <buwenson@gmail.com>
2019-02-08 13:27:29 -0800

    Bar plot edits

--- HW2/HW2_Benson.R
+++ HW2/HW2_Benson.R
@@ -13,15 +13,19 @@ library(reshape2)
 library(gridExtra)
 
 #Load Data
-df1<-read.table("videodata.txt", header = T, sep = "", dec = ".")
-df2<-read.table("videodata2.txt", header = T, sep="", dec=".")
+videodf1<-read.table("videodata.txt", header = T, sep = "", dec = ".")
+videodf2<-read.table("videodata2.txt", header = T, sep="", dec=".")
 
-df<-cbind(df1, df2)
+# Rename time column in videodf2 to time2
+names(videodf2)[names(videodf2) == 'time'] <- 'time2'
+
+#Bind the two dataframes
+df<-cbind(videodf1, videodf2)
 
 # added a new variable named like_binary; 1 = like to play, 0 = like to or never played
-df <- df%>%mutate(df, like_binary = ifelse(like == 2| like == 3, 1, 0))
+df <- df%>%mutate(like_binary = ifelse(like == 2| like == 3, 1, 0))
 # added a variable named played_prior; 1 = played before survey, 0 = didn't play
-df <- df%>%mutate(df, played_prior = ifelse(time > 0, 1, 0))
+df <- df%>%mutate(played_prior = ifelse(time > 0, 1, 0))
 
 ######################
 ####Data managment####
@@ -52,7 +56,8 @@ df_sex <- df %>% select("sex", "likesgames")
 df_sex %>% na.omit(likesgames) %>% 
   ggplot(aes(factor(likesgames),..count..)) + 
   geom_bar(aes(fill = factor(sex)), position = "dodge") +
-  scale_x_discrete(labels=c("0" = "No", "1" = "No")) + xlab("Likes Games")
+  scale_x_discrete(labels=c("0" = "No", "1" = "No")) + xlab("Likes Games") +
+  guides(fill=guide_legend(title="sex"))
 
 #Now loop through them 
 
@@ -60,10 +65,19 @@ varlist<-c("sex", "home")
 for(i in varlist){
   eval(parse(text=paste0("df_",i," <- df %>% select('",i,"', 'likesgames')
   plot_likesgames_",i,"<-df_",i," %>% na.omit(likesgames) %>% 
-    ggplot(aes(factor(likesgames),..count..)) + 
+    ggplot(aes(factor(likesgames),y=..count.. / sum(..count..))) + 
     geom_bar(aes(fill = factor(",i,")), position = 'dodge') +
     scale_x_discrete(labels=c('0' = 'No', '1' = 'Yes')) + xlab('Likes Games')")))
 }
+
+
+plot_likesgames_home <-plot_likesgames_home +
+  scale_fill_discrete(name="Has computer\nat home",
+                      breaks=c("0", "1"),
+                      labels=c("No", "Yes"))
+plot_likes
+
+
 
 grid.arrange(plot_likesgames_home, plot_likesgames_sex, ncol=2)
 
