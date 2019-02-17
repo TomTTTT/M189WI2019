50f5201555413e7d9e2ffa184d560712518c90ad
benson-wu <44241658+benson-wu@users.noreply.github.com>
2019-02-17 00:52:50 -0800

    Initial Commit

---
+++ HW3/HW3_MasterCopy.R
@@ -0,0 +1,23 @@
+# Load working directory
+# Enter your own path in the quotes in the path variable
+# path<-"/Users/Timlee/"
+# setwd("C:/Users/Aiden/Desktop/UCSD/homework/math_189/case1/M189WI2019/")
+# setwd('C:/Users/buwen/OneDrive/Desktop/MATH 189/HW2')
+# path<-"C:/Users/buwen/"
+setwd(paste0(path,'Documents/Git/M189WI2019/HW3'))
+
+#Load dependencies
+library(dplyr)
+library(ggplot2)
+library(reshape2)
+library(gridExtra)
+library(stargazer)
+library(tree)
+library(rattle)
+library(rpart)
+library(rpart.plot)
+library(party)
+library(ggsci)
+
+#Load Data
+df<-read.table("data1.txt", header = T, sep = "", dec = ".")
