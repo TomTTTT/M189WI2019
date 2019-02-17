39d348ed5d703fca8a292e2628ceeff35fd590cc
aclark760 <46981877+aclark760@users.noreply.github.com>
2019-02-16 17:45:03 -0800

    Added Scenario 3 and An extra hypothesis

--- HW2/HW2_Austin.R
+++ HW2/HW2_Austin.R
@@ -118,6 +118,100 @@ chisq.test(Q1table)
 #####################
 #####Question 3######
 #####################
+#need to build a matrix this is people that play games
+#monthly or more/ played games in the last week
+df_likesgames_subset <- subset(df, time>0 |freq<=3)
+df_notLikeGames_subset <- subset(df, freq==4 & time==0|is.na(df$freq))
+
+ggplot(df_likesgames_subset, aes(x=time)) + geom_histogram()
+
+
+xbar=mean(df$time)
+xbarLikesGames= mean(df_likesgames_subset$time)
+
+
+placehold= 0
+for(i in 1:length(df$time)){placehold= placehold+(xbar-df$time[i])^2}
+sampleVar= placehold/(length(df$time)-1)
+
+unbiasedSampleVar=sampleVar*(314-1)/(314)
+unbiasedSampleSD= sqrt(unbiasedSampleVar)
+
+interval = c(xbar-2*unbiasedSampleSD/sqrt(length(df$time)),xbar+2*unbiasedSampleSD/sqrt(length(df$time)))
+interval
+
+##interval only looking at people that play games
+placehold= 0
+for(i in 1:length(df_likesgames_subset$time)){placehold= placehold+(xbar-df_likesgames_subset$time[i])^2}
+sampleVarLikesGames= placehold/(length(df_likesgames_subset$time)-1)
+
+ubSampleVarLikesG=sampleVarLikesGames*(314-1)/(314)
+ubSampleSDLikesG= sqrt(ubSampleVarLikesG)
+
+intervalLikesGames = c(xbarLikesGames-2*ubSampleSDLikesG/sqrt(length(df_likesgames_subset$time))
+                       ,xbarLikesGames+2*ubSampleSDLikesG/sqrt(length(df_likesgames_subset$time)))
+intervalLikesGames
+
+
+#########################
+##Additional Hypothesis##
+#########################
+ggplot(df_likesgames_subset, aes(x=grade)) + geom_histogram()
+ggplot(df_notLikeGames_subset, aes(x=grade)) + geom_histogram()
+
+LikesA=0
+LikesB=0
+LikesC=0
+LikesDF=0
+
+for(i in 1:length(df_likesgames_subset$grade)){
+  if(df_likesgames_subset$grade[i]==4){LikesA= LikesA+1}
+  else if(df_likesgames_subset$grade[i]==3){LikesB= LikesB+1}
+  else if(df_likesgames_subset$grade[i]==2){LikesC= LikesC+1}
+  else{LikesDF=LikesDF+1}
+}
+
+DnLikeA=0
+DnLikeB=0
+DnLikeCDF=0
+
+for(i in 1:length(df_notLikeGames_subset$grade)){
+  if(df_notLikeGames_subset$grade[i]==4){DnLikeA = DnLikeA+1}
+  else if(df_notLikeGames_subset$grade[i]==3){DnLikeB = DnLikeB+1}
+  else{DnLikeCDF = DnLikeCDF+1}
+}
+
+
+ELikesA=as.integer(.2*length(df_likesgames_subset$grade))
+ELikesB=as.integer(.3*length(df_likesgames_subset$grade)+1)
+ELikesC=as.integer(.4*length(df_likesgames_subset$grade))
+ELikesDF=as.integer(.1*length(df_likesgames_subset$grade)+1)
+
+EDnLikeA=as.integer(.2*length(df_notLikeGames_subset))
+EDnLikeB=as.integer(.3*length(df_notLikeGames_subset))
+EDnLikeCDF=as.integer(.5*length(df_notLikeGames_subset)+1)
+
+
+ExtraTable1= matrix(c(LikesA,ELikesA,LikesB,ELikesB,LikesC,ELikesC,LikesDF,ELikesDF),ncol = 2,byrow = TRUE)
+colnames(ExtraTable1)= c("observed", "expected")
+rownames(ExtraTable1)= c("A","B", "C","D/F")
+ExtraTable1=as.table(ExtraTable1)
+ExtraTable1
+chisq.test(ExtraTable1)
+
+ExtraTable2= matrix(c(DnLikeA,EDnLikeA,DnLikeB,EDnLikeB,DnLikeCDF,EDnLikeCDF),ncol = 2,byrow = TRUE)
+colnames(ExtraTable2)= c("observed", "expected")
+rownames(ExtraTable2)= c("A","B", "C/D/F")
+ExtraTable2=as.table(ExtraTable2)
+ExtraTable2
+chisq.test(ExtraTable2)
+
+v1=c(LikesA,LikesB,LikesC+LikesDF)
+v2=c(DnLikeA,DnLikeB,DnLikeCDF)
+
+ExtraTable3= matrix(c(DnLikeA,LikesA,DnLikeB,LikesB,DnLikeCDF,LikesC+LikesDF),ncol = 2,byrow = TRUE)
+chisq.test(ExtraTable3)
+ks.test(v1,v2)
 
 
 
