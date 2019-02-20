c12bc061057e2ce0b1ae6642b820e67b996700bf
Benson Wu <buwenson@gmail.com>
2019-02-19 21:33:53 -0800

    Initial commit

---
+++ HW3/HW3_MasterCopy.R
@@ -0,0 +1,26 @@
+# Load working directory
+# Enter your own path in the quotes in the path variable
+# path<-"/Users/Timlee/"
+# setwd("C:/Users/Aiden/Desktop/UCSD/homework/math_189/case1/M189WI2019/")
+# setwd('C:/Users/buwen/OneDrive/Desktop/MATH 189/HW2')
+# path<-"C:/Users/buwen/"
+setwd(paste0(path,'Documents/Git/M189WI2019/HW3'))
+
+#Load dependencies
+library(dplyr)
+library(ggplot2)
+library(reshape2)
+library(gridExtra)
+library(stargazer)
+library(tree)
+library(rattle)
+library(rpart)
+library(rpart.plot)
+library(party)
+library(ggsci)
+
+#Load Data
+df<-read.csv("Data1.txt")
+
+#Plot with bin size = 4000
+ggplot(df, aes(x=location)) + geom_histogram(binwidth = 4000)
