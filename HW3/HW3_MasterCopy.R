f34e85fdc9350ec5f717f5797ef309cd47ab7002
AidenYoon <ajy030@ucsd.edu>
2019-02-20 16:42:16 -0800

    Added column that gives distance between palindromes

--- HW3/HW3_MasterCopy.R
+++ HW3/HW3_MasterCopy.R
@@ -1,7 +1,7 @@
 # Load working directory
 # Enter your own path in the quotes in the path variable
 # path<-"/Users/Timlee/"
-# setwd("C:/Users/Aiden/Desktop/UCSD/homework/math_189/case1/M189WI2019/")
+# setwd("C:/Users/Aiden/Documents/GitHub/M189WI2019/HW3")
 # setwd('C:/Users/buwen/OneDrive/Desktop/MATH 189/HW2')
 # path<-"C:/Users/buwen/"
 setwd(paste0(path,'Documents/Git/M189WI2019/HW3'))
@@ -18,9 +18,15 @@ library(rpart)
 library(rpart.plot)
 library(party)
 library(ggsci)
+library(Hmisc)
 
 #Load Data
 df<-read.csv("Data1.txt")
 
 #Plot with bin size = 4000
 ggplot(df, aes(x=location)) + geom_histogram(binwidth = 4000)
+
+#Create column that give distance between Palindrome
+df$location_lag <- Lag(df$location, 1)
+df$distance <- df$location-df$location_lag
+df$location_lag<-NULL
