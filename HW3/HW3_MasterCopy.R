de2d0f3368e58e86cbeacf7cb90a19cb70ac2297
Benson Wu <buwenson@gmail.com>
2019-03-01 18:38:29 -0800

    Part 3 almost finalized. Just need create kable tables. Additional hypothesis almost done as well; need to fix plots

--- HW3/HW3_MasterCopy.R
+++ HW3/HW3_MasterCopy.R
@@ -24,6 +24,7 @@ library(knitr)
 library(purrr)
 library(OneR)
 library(nnet)
+library(dplyr)
 
 #Load Data
 df<-read.csv("Data1.txt")
@@ -245,28 +246,141 @@ grid.arrange(df_pair, rexp_df, rexp_unifMC_1,rexp_unifMC_2,rexp_unifMC_3,
 ######################################
 
 
+####Part 3
 #These are the MLE for lambda with different bin sizes 
 cut_3000<-as.data.frame(table(cut(df$location, breaks=seq(0,229354, by=3000), dig.lab =7)))
 c3m<-round(mean(cut_3000$Freq),2)
-#3.87
+bin_val_3000 <- trunc(229354/3000)
+freq_3000 <- as.data.frame(table(cut_3000$Freq))
+#Generating Frequency Chart for 3000
+expected <- c()
+for(i in 0:30){
+  expected <- append(expected,dpois(i, c3m) * bin_val_3000)
+}
 
+freq_3000$Var1 <- as.numeric(freq_3000$Var1)
+
+Poison_3_table <- (data.frame("Var" = 0:30,
+                              "Expected" = expected)) %>% left_join(freq_3000, by = c("Var" = "Var1"))
+Poison_3_table[is.na(Poison_3_table)] <- 0
+
+#Poisson 3000
+Poison_3_table$Var <- as.character(Poison_3_table$Var)
+cleaned_poisson_3 <- rbind(data.frame("Var" = "0-1", "Expected" = 7.7201, "Freq" = 1),
+                           Poison_3_table[3:7,],
+                           data.frame("Var" = "7+", "Expected" = 7.415565, "Freq" = 16))
+
+c3m_chi_square_val <- 0 
+for (i in 1:nrow(cleaned_poisson_3)){
+  expected_val <- cleaned_poisson_3[i,2]
+  diff <- cleaned_poisson_3[i,3] - expected_val
+  c3m_chi_square_val <- c3m_chi_square_val + (diff^2)/expected_val
+}
+
+
+#4500
 cut_4500<-as.data.frame(table(cut(df$location, breaks=seq(0,229354, by=4500), dig.lab =7)))
 c4m<-round(mean(cut_4500$Freq),2)
+freq_4500 <- as.data.frame(table(cut_4500$Freq))
+bin_val_4500 <- trunc(229354/4500)
+
+#Generating Frequency Chart for 4500
+expected <- c()
+for(i in 0:30){
+  expected <- append(expected,dpois(i, c4m) * bin_val_4500)
+}
+
+freq_4500$Var1 <- as.numeric(freq_4500$Var1)
+
+Poison_4_table <- (data.frame("Var" = 0:30,
+                              "Expected" = expected)) %>% left_join(freq_4500, by = c("Var" = "Var1"))
+Poison_4_table[is.na(Poison_4_table)] <- 0
+
+#Poisson 4500
+Poison_4_table$Var <- as.character(Poison_4_table$Var)
+cleaned_poisson_4 <- rbind(data.frame("Var" = "0-3", "Expected" = 8.597073, "Freq" = 12 ),
+                           Poison_4_table[5:8,],
+                           data.frame("Var" = "8+", "Expected" = 11.31634, "Freq" = 12))
+
+c4m_chi_square_val <- 0 
+for (i in 1:nrow(cleaned_poisson_4)){
+  expected_val <- cleaned_poisson_4[i,2]
+  diff <- cleaned_poisson_4[i,3] - expected_val
+  c4m_chi_square_val <- c4m_chi_square_val + (diff^2)/expected_val
+}
+
+
+
 #5.78
 
 cut_6000<-as.data.frame(table(cut(df$location, breaks=seq(0,229354, by=6000), dig.lab =7)))
 c6m<-round(mean(cut_6000$Freq),2)
+freq_6000 <- as.data.frame(table(cut_6000$Freq))
+bin_val_6000 <- trunc(229354/6000)
+
+#Generating Frequency Chart for 6000
+expected <- c()
+for(i in 0:30){
+  expected <- append(expected,dpois(i, c6m) * bin_val_6000)
+}
+
+freq_6000$Var1 <- as.numeric(freq_6000$Var1)
+
+Poison_6_table <- (data.frame("Var" = 0:30,
+                              "Expected" = expected)) %>% left_join(freq_6000, by = c("Var" = "Var1"))
+Poison_6_table[is.na(Poison_6_table)] <- 0
+
+#Poisson 6000
+Poison_6_table$Var <- as.character(Poison_6_table$Var)
+cleaned_poisson_6 <- rbind(data.frame("Var" = "0-5", "Expected" = 13.15367, "Freq" = 20),
+                           Poison_6_table[8:9,],
+                           data.frame("Var" = "9-10", "Expected" = 8.05756, "Freq" = 5),
+                           data.frame("Var" = "11+", "Expected" = 6.048498, "Freq" = 5))
+
+c6m_chi_square_val <- 0 
+for (i in 1:nrow(cleaned_poisson_6)){
+  expected_val <- cleaned_poisson_6[i,2]
+  diff <- cleaned_poisson_6[i,3] - expected_val
+  c6m_chi_square_val <- c6m_chi_square_val + (diff^2)/expected_val
+}
+
 #7.74
 
 cut_10000<-as.data.frame(table(cut(df$location, breaks=seq(0,229354, by=10000), dig.lab =7)))
 c10m<-round(mean(cut_10000$Freq),2)
-#12.73
+freq_10000 <- as.data.frame(table(cut_10000$Freq))
+bin_val_10000 <- trunc(229354/10000)
+
+#Generating Frequency Chart for 10000
+expected <- c()
+for(i in 0:30){
+  expected <- append(expected,dpois(i, c10m) * bin_val_10000)
+}
+
+freq_10000$Var1 <- as.numeric(freq_10000$Var1)
+
+Poison_10_table <- (data.frame("Var" = 0:30,
+                               "Expected" = expected)) %>% left_join(freq_10000, by = c("Var" = "Var1"))
+Poison_10_table[is.na(Poison_10_table)] <- 0
+
+
+
+##### CALCULATING CHI SQUARE VALUES #####
+pchisq(c3m_chi_square_val, nrow(cleaned_poisson_3) - 2, lower.tail = FALSE) 
+pchisq(c4m_chi_square_val, nrow(cleaned_poisson_4) - 2, lower.tail = FALSE)
+pchisq(c6m_chi_square_val, nrow(cleaned_poisson_6) - 2, lower.tail = FALSE)
+#interval lengths: 3000,4500,6000; chi^2 test pvalue: the numbers above
+
+
+
+#######
+#Put cleaned_poisson_* into tables to publish
 
 #Table that organizes these Poisson MLE results
 Poisson_MLE_df <- data.frame(binwidth = c(3000, 4500, 6000, 10000),
                      value = c(c3m, c4m, c6m, c10m))
 
-knitr::kable(MLE_df,
+knitr::kable(Poisson_MLE_df,
              row.names = FALSE,
              col.names = c("Bin Width", "$\\hat{\\lambda}$"), booktabs = TRUE,
              caption = "Poisson MLE of $\\hat{\\lambda}$", escape = FALSE)
@@ -277,25 +391,66 @@ knitr::kable(MLE_df,
 
 
 
-
-#####Addtional hypothesis (need to fix)
+###############################
+#####Addtional hypothesis######
+###############################
 df2<-read.csv("Data2.csv")
 
 #Create binaries
+#HIV status
 df2$hiv_binary <- ifelse(df2$hiv == "positive", 1, 0)
+df2$hiv_binary <- factor(df2$hiv_binary)
+
+#Sex
 df2$sex<-ifelse(df2$"ï..sex" =="Male",1,0) #Male 1; Female 0
 df2$sex <- factor(df2$sex)
 
+#4 category CMV
 df2$cmv_tertile_4cat <- ifelse(df2$cmv_tertile == "Negative", 0,
                                ifelse(df2$cmv_tertile == "Low", 1,
                                       ifelse(df2$cmv_tertile == "Medium", 2, 3)))
 df2$cmv_tertile_4cat<-factor(df2$cmv_tertile_4cat)
-test21<-glm(hiv_binary~cmv_tertile_4cat, data=df2, family=binomial())
-test21predict<-as.data.frame(predict(test21))
 
-HCMV IgG OD (as the dependent variable) and sex, HIV and TB status 
-test9<-glm(cmv~, data=df2) 
-             #sex + hiv_binary, data=df2)
-test9<-predict(test9)
-df3<-cbind(df2, test9)
+#CMV Status 
+df2$cmvstatus_binary <- ifelse(df2$cmvstatus == "positive", 1, 0)
+df2$cmvstatus_binary <- factor(df2$cmvstatus_binary)
+
+#Make age continuous
+df2$age_yrs<-as.numeric(as.character(df2$age_yrs))
+
+#Factorize blood pressure variables
+df2$mean_syst<-as.numeric(as.character(df2$mean_syst))
+df2$mean_diast<-as.numeric(as.character(df2$mean_diast))
+
+#Loop for categorical variables
+traits_c<-c("hiv", "R22_bpgroup", "bmi")
+
+df3 <- subset(df2, is.na(df2$hiv))
+df3 <- subset(df3, is.na(df2$R22_bpgroup))
+
+#Relevel variables to reorder boxplots 
+#HIV
+df3$hiv_reordered <- factor(df3$hiv,
+                       levels = c('unknown','negative','positive'),ordered = TRUE)
+#Hypertension
+df2$R22_bpgroup[(df2$R22_bpgroup!="Hypertension stage 1") & 
+                  (df2$R22_bpgroup!="Hypertension stage 2") & 
+                  (df2$R22_bpgroup!="Normal") &
+                  (df2$R22_bpgroup!="Pre-hypertension")]<-"Unknown"
+
+df2$R22_bpgroup_reordered <- factor(df2$R22_bpgroup,
+                                    levels - c("", "Hypertension stage 1", "Hypertension stage 2", "Normal", "Pre-hypertension"), ordered=TRUE)
+
+
+#box plots 
+box_hiv<-ggplot(subset(df2, !is.na(hiv_reordered)), aes(x=hiv_reordered, y=cmv)) + geom_boxplot(color="black", fill="red", alpha=0.2)
+
+for(i in traits_c){
+  eval(parse(text=paste0("
+  box_",i,"<-ggplot(df2, aes(x=",i,", y=cmv)) + geom_boxplot()
+")))
+}
+
+grid.arrange(box_bmi, box_hiv, box_R22_bpgroup, ncol=2)
+
 
