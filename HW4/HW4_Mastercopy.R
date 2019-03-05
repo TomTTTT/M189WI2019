c97aa0a6168b6f52af89fe87e6af99fe42e4f2f1
Benson Wu <buwenson@gmail.com>
2019-03-04 19:53:42 -0800

    intial commit

---
+++ HW4/HW4_Mastercopy.R
@@ -0,0 +1,30 @@
+# Load working directory
+# Enter your own path in the quotes in the path variable
+# path<-"/Users/Timlee/"
+# setwd("C:/Users/Aiden/Documents/GitHub/M189WI2019/HW3")
+# setwd('C:/Users/buwen/OneDrive/Desktop/MATH 189/HW2')
+# path<-"C:/Users/buwen/"
+setwd(paste0(path,'Documents/Git/M189WI2019/HW4'))
+
+#Load dependencies
+library(dplyr)
+library(ggplot2)
+library(reshape2)
+library(gridExtra)
+library(stargazer)
+library(tree)
+library(rattle)
+library(rpart)
+library(rpart.plot)
+library(party)
+library(ggsci)
+library(Hmisc)
+library(lattice)
+library(knitr)
+library(purrr)
+library(OneR)
+library(nnet)
+library(dplyr)
+
+#Load Data
+df<-read.csv("data1.txt")
