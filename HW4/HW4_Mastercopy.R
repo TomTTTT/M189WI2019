b3e04d910f3d1d6cb8e2202d5b7370410ce05501
AidenYoon <ajy030@ucsd.edu>
2019-03-06 17:17:11 -0800

    added ribbon

--- HW4/HW4_Mastercopy.R
+++ HW4/HW4_Mastercopy.R
@@ -1,7 +1,7 @@
 # Load working directory
 # Enter your own path in the quotes in the path variable
 # path<-"/Users/Timlee/"
-# setwd("C:/Users/Aiden/Documents/GitHub/M189WI2019/HW3")
+# setwd("C:/Users/Aiden/Documents/GitHub/M189WI2019/HW4")
 # setwd('C:/Users/buwen/OneDrive/Desktop/MATH 189/HW2')
 # path<-"C:/Users/buwen/"
 setwd(paste0(path,'Documents/Git/M189WI2019/HW4'))
@@ -62,11 +62,16 @@ glm_log<-glm(loggain~density, data=df, family=gaussian())
 df$glm_log_residual<-resid(glm_log)
 
 summary(glm_log)
+loggain_residual <- as.data.frame(summary(glm_log)$coefficients[,2])
+loggain_mean <- mean(df$glm_log_residual)
 
 #RESIDUAL PLOT
 #Linear
 ggplot(df, aes(x=density, y=glm_residual)) + geom_point()
 
 #Log
-ggplot(df, aes(x=density, y=glm_log_residual)) + geom_point()
+ggplot(df, aes(x=density, y=glm_log_residual)) + geom_point() +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
+  geom_ribbon(aes(ymin= -loggain_residual[2,1],ymax=loggain_mean+loggain_residual[2,1]),alpha=0.2,fill="red")
 #This residual plot tells you that the data is not normal
+
